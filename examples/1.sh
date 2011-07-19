293eddc253be2518ce7e6bdc142340011e2643ce
Timbo White <tim.white@zulius.com>
2011-07-18 21:51:38 -0700

    Adding/updating examples

--- examples/1.sh
+++ examples/1.sh
@@ -6,6 +6,83 @@ CWD=`dirname $0`
 # source the library file
 . "${CWD}/../bashtasklog.sh"
 
-printTask "Executing arbitrary code"
-sleep 2
+printTask -d "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
 printOk
+
+printTask -d "Sed sagittis rhoncus nisl, eu commodo risus facilisis sed."
+printOk
+
+printTask -d "Duis non quam felis, et auctor risus."
+printOk
+
+printTask -d "Nullam condimentum elit et nulla scelerisque ultrices."
+printOk
+
+printTask -d "Duis sed orci elit, non tristique nulla."
+printOk
+
+printTask -d "Cras tempus tellus sed odio dapibus elementum."
+printOk
+
+printTask -d "Maecenas mattis ullamcorper sem, id tristique est rhoncus at."
+printOk
+
+printTask -d "Pellentesque sed eros ante, eget vestibulum neque."
+printOk
+
+printTask -d "Donec sodales ultrices purus, vitae molestie nisl elementum non."
+printOk
+
+printTask -d "Ut ullamcorper tristique nisi, vitae auctor libero malesuada id."
+printOk
+
+printTask -d "Sed faucibus massa orci, et vulputate sem."
+printOk
+
+printTask -d "Fusce luctus felis interdum risus faucibus rutrum."
+printOk
+
+printTask -d "Integer tempus ante quis orci pharetra lobortis."
+printOk
+
+printTask -d "Proin tincidunt eleifend magna, ac bibendum ante lacinia commodo."
+printOk
+
+printTask -d "Sed et quam risus, eget pulvinar nulla."
+printOk
+
+printTask -d "Suspendisse id metus vitae justo aliquet mattis."
+printOk
+
+printTask -d "Vivamus ullamcorper nulla a lorem congue interdum."
+printOk
+
+printTask -d "Donec ac velit ac quam elementum bibendum sit amet vel mi."
+printOk
+
+printTask -d "Vivamus vulputate ornare ipsum, ac aliquet ligula tristique eget."
+printOk
+
+printTask -d "Nunc cursus venenatis urna, iaculis congue orci vestibulum vitae."
+printOk
+
+printTask -d "Cras convallis condimentum est, eu facilisis nisi consectetur quis."
+printOk
+
+printTask -d "Suspendisse commodo sem vel nisi consequat in suscipit nulla porttitor."
+printOk
+
+printTask -d "Nullam tincidunt turpis nec est pellentesque pharetra."
+printOk
+
+printTask -d "Sed feugiat libero sed ante pellentesque ullamcorper."
+printOk
+
+printTask -d "Duis id massa vel elit tempus fringilla venenatis eu sem."
+printOk
+
+
+
+# print a spacer
+echo -e "\n\n"
+
